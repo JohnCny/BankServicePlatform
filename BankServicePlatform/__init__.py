7febf40d3c96eb7f530574aab0f15247a963a7c0
Johnny <jianghanshi@vip.qq.com>
2017-08-04 23:15:23 +0800

    修改db

--- BankServicePlatform/__init__.py
+++ BankServicePlatform/__init__.py
@@ -3,6 +3,8 @@ __author__ = 'Johnny'
 
 import sys
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -11,3 +13,6 @@ app = Flask(__name__)
 
 # 读取配置文件
 app.config.from_object('BankServicePlatform.settings.ProConfig') # mysql
+
+# 初始化数据库
+db = SQLAlchemy(app)
