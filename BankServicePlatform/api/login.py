8f3583b6925dbaa96ec80994ad4767788c26abbd
Johnny <jianghanshi@vip.qq.com>
2017-08-13 16:53:23 +0800

    [mod] 微信服务号接口测试

--- BankServicePlatform/api/login.py
+++ BankServicePlatform/api/login.py
@@ -54,7 +54,7 @@ def get_token():
 
 def get_wx_openid():
     #获取用户code
-    url="https://bsp.qkjr.com.cn/wdzh"
+    url="http://bsp.qkjr.com.cn/wdzh"
     url=url.decode('gbk','replace')
     redirect_uri=urllib.quote(url.encode('utf8','replace'))
     CODE_URL="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8ca1ef28740b0106" \
