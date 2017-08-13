1953721f45197c525a33cee925d46442aa990193
Johnny <jianghanshi@vip.qq.com>
2017-08-13 16:15:23 +0800

    [mod] 微信服务号接口测试

--- BankServicePlatform/api/login.py
+++ BankServicePlatform/api/login.py
@@ -3,7 +3,11 @@
 #
 from flask import Blueprint,request,g
 
+import json
+
 from . import route_nl
+
+import urllib2 as urllib
 
 from ..core import  redis
 
@@ -32,7 +36,36 @@ def login():
 
 @route_nl(bp,'/get_token',methods=['GET'])
 def get_token():
-    print(dict(request.data))
-    print(dict(request.values))
-    openid=dict(**request.json)['openid']
+    # access_token=redis.get("access_token",None)
+    # if access_token:
+    #     openid=dict(**request.json)['openid']
+    # else:
+    #     response_at=urllib.urlopen('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx8ca1ef28740b0106&secret=727125a17d9dd9d2508e7d3c46c85fbb')
+    #     access_token_get=json.loads(response_at.read()).get('access_token',None)
+    #     if access_token_get:
+    #         expires_in=json.loads(response_at.read()).get('expires_in',None)
+    #         redis.set("access_token",access_token_get,ex=expires_in)
+    #
+    #         openid=dict(**request.json)['openid']
+    #     else:
+    #         return "GET WX_ACCESS_TOKEN FAILED",500
+    openid=get_wx_openid()
     return redis.get(openid)
+
+def get_wx_openid():
+    #获取用户code
+    url="https://bsp.qkjr.com.cn/wdzh"
+    url=url.decode('gbk','replace')
+    redirect_uri=urllib.quote(url.encode('utf8','replace'))
+    CODE_URL="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8ca1ef28740b0106" \
+             "&redirect_uri="+redirect_uri+"&response_type=code&scope=snsapi_base"
+    response=urllib.urlopen(CODE_URL)
+    code=json.loads(response.read()).get('code',None)
+    print code
+    #获取用户openid
+    OPENID_URL="https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx8ca1ef28740b0106&secret=727125a17d9dd9d2508e7d3c46c85fbb" \
+               "&code="+code+"&grant_type=authorization_code"
+    response_oi=urllib.urlopen(OPENID_URL)
+    openid=json.loads(response_oi.read()).get('openid',None)
+    print openid
+    return openid
