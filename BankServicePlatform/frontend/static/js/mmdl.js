37d0d18b7c5eacee27a47b489cecf55d5cb75948
tanwh <tan_w_h@sina.com>
2017-08-14 18:43:34 +0800

    [tmp]

--- BankServicePlatform/frontend/static/js/mmdl.js
+++ BankServicePlatform/frontend/static/js/mmdl.js
@@ -1,14 +1,18 @@
-var Customers = Backbone.Collection.extend({
+var Customer =  Backbone.Model.extend({
     url: '/api/login',
-    parse: function(response) {
-        //if(response.data.success)
-        localStorage.setItem(appid, response.data)
-        window.location.href = "wdzh";
+    //解析异步请求返回的结果，fetch方法与save方法都会调用它
+    parse: function(res) {
+        //保存customeId和token
+        //localStorage.setItem(key_customer, res.data.customer.id);
+        //localStorage.setItem(key_token, res.data.token);
+
+        localStorage.setItem(key_customer_id, 1);
+        localStorage.setItem(key_token, "111");
+        window.location.href = 'wdzh';
     }
 });
 
-var customers = new Customers;
-
+var customer = new Customer;
 $("#subBtn").click(function() {
-    customers.create(JSON.stringify($('#contentForm').serializeJSON()));
+    customer.save($('#contentForm').serializeJSON());
 });
--- BankServicePlatform/frontend/static/js/zc.js
+++ BankServicePlatform/frontend/static/js/zc.js
@@ -2,28 +2,18 @@ var Customer =  Backbone.Model.extend({
     url: '/api/customer',
     //解析异步请求返回的结果，fetch方法与save方法都会调用它
     parse: function(res) {
-        return res.data;
+        //保存customeId和token
+        //localStorage.setItem(key_customer_id, res.data.customer.id);
+        //localStorage.setItem(key_token, res.data.token);
+        localStorage.setItem(key_customer_id, 1);
+        localStorage.setItem(key_token, "111");
+        window.location.href = 'wdzh';
     }
 });
 
-var Customers = Backbone.Collection.extend({
-    url: '/api/customer',
-    parse: function(response) {
-        alert(111)
-        window.location.href = "wdzh";
-    }
-});
-
-var customers = new Customers;
-
+var customer = new Customer;
 $("#subBtn").click(function() {
-    //var obj = new Object();
-    //obj.customer = $('#contentForm').serializeJSON();
-    //var data = { "customer": JSON.stringify($('#contentForm').serializeJSON()) };
-    //alert(data);
-
-    var data = '{ "customer":' + JSON.stringify($('#contentForm').serializeJSON());
-    var customer = new Customer;
-    //customer.set({ "customer": JSON.stringify($('#contentForm').serializeJSON()) });
-    customer.save(data);
+    var obj = [];
+    obj["customer"] = $('#contentForm').serializeJSON();
+    customer.save(obj);
 });
