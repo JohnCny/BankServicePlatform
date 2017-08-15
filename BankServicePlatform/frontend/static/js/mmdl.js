972074dbac54147d7c6f024b24c4ae8a66cca2a3
tanwh <tan_w_h@sina.com>
2017-08-15 13:14:32 +0800

    [mod]

--- BankServicePlatform/frontend/static/js/mmdl.js
+++ BankServicePlatform/frontend/static/js/mmdl.js
@@ -3,13 +3,17 @@ var Customer =  Backbone.Model.extend({
     url: '/api/login',
     //解析异步请求返回的结果，fetch方法与save方法都会调用它
     parse: function(res) {
-        //保存customeId和token
-        localStorage.setItem(key_customer_id, res.data.customer.id);
-        localStorage.setItem(key_token, res.data.token);
-        //localStorage.setItem(key_customer_id, 1);
-        //localStorage.setItem(key_token, "eyJhbGciOiJIUzI1NiIsImV4cCI6MTUwMjc5MjM4NCwiaWF0IjoxNTAyNzA1OTg0fQ.eyJpZCI6NjZ9.r2Uxsm_5YDKdMNrlUof4bYr01qL-w6T_He4NdJnq6jg");
+        if (res.result == "Success") {
+            //保存customeId和token
+            localStorage.setItem(key_customer_id, res.data.customer.id);
+            localStorage.setItem(key_token, res.data.token);
+            //localStorage.setItem(key_customer_id, 1);
+            //localStorage.setItem(key_token, "eyJhbGciOiJIUzI1NiIsImV4cCI6MTUwMjc5MjM4NCwiaWF0IjoxNTAyNzA1OTg0fQ.eyJpZCI6NjZ9.r2Uxsm_5YDKdMNrlUof4bYr01qL-w6T_He4NdJnq6jg");
 
-        window.location.href = 'wdzh';
+            window.location.href = 'wdzh';
+        } else {
+            alert(res.info)
+        }
     }
 });
 
